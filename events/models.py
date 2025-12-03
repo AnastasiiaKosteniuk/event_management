5ae7df94341cd87dd8c973f4a490e2c5f197d43f
Anastasiia Kosteniuk <nastuakostenyk@gmail.com>
2025-12-03 16:48:20 +0200

    add Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    organizer = models.ForeignKey(User, on_delete=models.PROTECT, related_name='organized_events')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
