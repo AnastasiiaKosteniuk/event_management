b790e86d07806135db5c9b305f87db4180bd3a3d
Anastasiia Kosteniuk <nastuakostenyk@gmail.com>
2025-12-03 22:41:59 +0200

    add permissions for event crud operations

--- events/views.py
+++ events/views.py
@@ -1,13 +1,21 @@
 from rest_framework import viewsets, permissions
 
 from events.models import Event
+from events.permissions import IsOrganizer
 from events.serializers import EventSerializer
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.select_related("organizer").order_by("-date")
     serializer_class = EventSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            # updating/deleting - only organizer + authenticated
+            return [permissions.IsAuthenticated(), IsOrganizer()]
+        else:
+            # viewing/creating events - any authenticated user
+            return [permissions.IsAuthenticated()]
 
     def perform_create(self, serializer):
         serializer.save(organizer=self.request.user)
